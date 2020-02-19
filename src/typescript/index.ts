7ac6e2620380d9dddf20ba056adf011a69957acd
etiennefluhr <etienne.fluhr@arcor.de>
2020-02-19 14:01:46 +0100

    Added initial npm sceleton.

---
+++ src/typescript/index.ts
@@ -0,0 +1,22 @@
+
+    // eslint-disable-next-line import/no-deprecated
+    // import './de/mayflower/bz/css/global.less';
+    // import * as bz from './de/mayflower/bz';
+
+    /** ****************************************************************************************************************
+    *   Being invoked when the page is loaded completely.
+    *******************************************************************************************************************/
+    window.onload = () : void  =>
+    {
+        // bz.Main.main();
+
+       console.log( "Welcome to the TypeScript Workshop" );
+    };
+
+    /** ****************************************************************************************************************
+    *   Being invoked when the page is left.
+    *******************************************************************************************************************/
+    window.onunload = () : void  =>
+    {
+        // no actions required on leaving website
+    };
